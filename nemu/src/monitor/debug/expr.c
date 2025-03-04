95774490055acdc00e2518ddcd5bd3cacb6c8b25
tracer-ics2017 <tracer@njuics.org>
2025-03-04 17:46:54 +0800

    >  compile 2213410 xu Linux xu-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:46:53 up 17:01,  1 user,  load average: 0.00, 0.04, 0.06 7379a095ee8ac63bd873ee598b8d2112b867b66b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -176,6 +176,10 @@ static int find_dominant_op(int p, int q) {
       paren_level--;
       continue;
     } else if (paren_level == 0 && is_operator(tokens[i].type)) {
+      if (tokens[i].type == '-' && (i == p || is_operator(tokens[i - 1].type) || tokens[op_pos - 1].type == '(')) {
+        // 跳过负号
+        continue;
+      }
       int priority = get_priority(tokens[i].type);
       if (priority < min_priority) {
         min_priority = priority;
@@ -256,7 +260,7 @@ static double eval(int p, int q, bool *success){
     printf("expr: val1 = %f, success = %d\n", val1, left_success);
     printf("expr: val2 = %f, success = %d\n", val2, right_success);
     
-    if (tokens[op_pos].type == '-' && (op_pos == p || is_operator(tokens[op_pos - 1].type) || tokens[op_pos - 1].type == '('))
+    if (tokens[p].type == '-')
         if (!right_success) {
           *success = false;
           return 0;
