6a72f110fac09ca9e826c86a6ff2811b17eee631
tracer-ics2017 <tracer@njuics.org>
2025-03-04 12:48:01 +0800

    >  compile 2213410 xu Linux xu-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  12:48:00 up 12:02,  1 user,  load average: 0.26, 0.22, 0.16 5a51e15714e68b18fb762612308bd8aedd9ae85d

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -165,6 +165,7 @@ static int cmd_p(char *args) {
 
   bool success = false;
   uint32_t value = expr(arg, &success);
+  printf("value: %d\n", value);
   if (success) {
     printf("表达式的值: %d\n", value);
   } else {
