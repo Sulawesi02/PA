2245cc0f5ff2524bab865383010cfd00d1bcdb6a
tracer-ics2017 <tracer@njuics.org>
2025-03-19 15:45:18 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:45:18 up 7 min,  1 user,  load average: 0.59, 0.50, 0.28 18da2ecb67b76c6ee70a3ee4f8a8921044049099

--- nexus-am/tests/cputest/run.sh
+++ nexus-am/tests/cputest/run.sh
@@ -7,7 +7,6 @@ export AM_HOME=/home/xu/share_projects/PA/ics2017/nexus-am
 # 使用 expect 自动输入 c
 expect << EOF
 spawn make ARCH=x86-nemu ALL=add run
-# 更灵活地匹配提示信息
 expect -re {\(nemu\)}
 send "c\r"
 expect eof
