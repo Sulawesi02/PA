2495f8833c7dbeecc7c11b09a26c928fb865a43f
tracer-ics2017 <tracer@njuics.org>
2025-03-19 23:55:41 +0800

    >  compile 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:55:41 up  7:46,  1 user,  load average: 0.83, 0.57, 0.62 b371554dcee59019d734efa8bc05d05e8038ff9c

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -15,36 +15,25 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:// 获取溢出标志位(OF)
-      rtl_get_OF(dest);
+      *dest = cpu.eflags.OF;
       break;
     case CC_B:// 获取进位标志位(CF)
-      rtl_get_CF(dest);
+      *dest = cpu.eflags.CF;
       break;
     case CC_E:// 获取零标志位(ZF)
-      rtl_get_ZF(dest);
+      *dest = cpu.eflags.ZF;
       break;
     case CC_BE:// 检查CF=1或ZF=1
-      assert(dest != &t0);
-      rtl_get_CF(dest);
-      rtl_get_ZF(&t0);
-      rtl_or(dest, dest, &t0);
+      *dest = cpu.eflags.CF | cpu.eflags.ZF;
       break;
     case CC_S:// 获取符号标志位(SF)
-      rtl_get_SF(dest);
+      *dest = cpu.eflags.SF;
       break;
     case CC_L:// 检查SF!=OF
-      assert(dest != &t0);
-      rtl_get_SF(dest);
-      rtl_get_OF(&t0);
-      rtl_xor(dest, dest, &t0);
+      *dest = cpu.eflags.SF != cpu.eflags.OF;
       break;
     case CC_LE:// 检查ZF=1或SF!=OF
-      assert(dest != &t0);
-      rtl_get_ZF(dest);
-      rtl_or(dest, dest, &t0);
-      rtl_get_SF(&t0);
-      rtl_get_OF(&t1);
-      rtl_xor(dest, &t0, &t1);
+      *dest = cpu.eflags.ZF || (cpu.eflags.SF != cpu.eflags.OF);
       break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
