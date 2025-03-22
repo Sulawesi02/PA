4556efdbf0678456f4f4d5733c150a751031b366
tracer-ics2017 <tracer@njuics.org>
2025-03-22 23:30:21 +0800

    >  compile 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:30:21 up  8:35,  1 user,  load average: 1.10, 0.87, 0.98 d7b23bf59a4d37b22bc52004d114aa7b91ecb958

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -140,8 +140,11 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
   rtl_li(&t1, 32 - width * 8);
+  printf("t1: 0x%08x\n", t1);
   rtl_shl(dest, src1, &t1);
+  printf("dest: 0x%08x\n", *dest);
   rtl_sar(dest, dest, &t1);
+  printf("dest: 0x%08x\n", *dest);
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
