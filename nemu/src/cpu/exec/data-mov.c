e5deff9d16960447c58053349601e83978239d78
tracer-ics2017 <tracer@njuics.org>
2025-03-22 17:54:43 +0800

    >  compile 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:54:42 up  3:53,  1 user,  load average: 0.34, 0.80, 0.92 55d1bc3bb64fcca479cd27598cb1d051ef2f8d82

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -38,36 +38,55 @@ make_EHelper(leave) {
   print_asm("leave");
 }
 
+// make_EHelper(cltd) {
+//   if (decoding.is_operand_size_16) {
+//     //TODO();
+//     // 将AX符号扩展到DX:AX
+//     rtl_lr(&t0, R_AX, 2); //加载AX到t0
+//     rtl_sext(&t0, &t0, 2); //符号扩展t0
+//     rtl_msb(&t1, &t0, 2); // 获取符号位
+
+//     if(t1 == 0){
+//       rtl_li(&t1, 0);
+//     }
+//     else{
+//       rtl_li(&t1, 0xffff);
+//     }
+//     rtl_sr(R_DX, 2, &t1); // 设置DX
+
+//   }
+//   else {
+//     //TODO();
+//     // 将EAX符号扩展到EDX:EAX
+//     rtl_lr(&t0, R_EAX, 4); //加载EAX到t0
+//     rtl_sext(&t0, &t0, 4); //符号扩展t0
+//     rtl_msb(&t1, &t0, 4); // 获取符号位
+//     if(t1==0){
+//       rtl_li(&t1, 0);
+//     }
+//     else{
+//       rtl_li(&t1, 0xffffffff);
+//     }
+//     rtl_sr(R_EDX, 4, &t1); // 设置EDX
+//   }
+
+//   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
+// }
+
 make_EHelper(cltd) {
   if (decoding.is_operand_size_16) {
     //TODO();
     // 将AX符号扩展到DX:AX
-    rtl_lr(&t0, R_AX, 2); //加载AX到t0
-    rtl_sext(&t0, &t0, 2); //符号扩展t0
-    rtl_msb(&t1, &t0, 2); // 获取符号位
-
-    if(t1 == 0){
-      rtl_li(&t1, 0);
-    }
-    else{
-      rtl_li(&t1, 0xffff);
-    }
-    rtl_sr(R_DX, 2, &t1); // 设置DX
-
+    t0 = reg_w(0);// 获取AX
+    rtl_sext(&t1, &t0, 2); // AX符号扩展
+    reg_w(2) = (uint16_t) ((uint32_t)t1 >> 16); // 取高16位给DX
   }
   else {
     //TODO();
     // 将EAX符号扩展到EDX:EAX
-    rtl_lr(&t0, R_EAX, 4); //加载EAX到t0
-    rtl_sext(&t0, &t0, 4); //符号扩展t0
-    rtl_msb(&t1, &t0, 4); // 获取符号位
-    if(t1==0){
-      rtl_li(&t1, 0);
-    }
-    else{
-      rtl_li(&t1, 0xffffffff);
-    }
-    rtl_sr(R_EDX, 4, &t1); // 设置EDX
+    t0 = reg_l(0); // 获取EAX
+    rtl_sext(&t1, &t0, 4); // EAX符号扩展
+    reg_l(2) = (uint32_t) ((uint64_t)t1 >> 32); // 取高32位给EDX
   }
 
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
