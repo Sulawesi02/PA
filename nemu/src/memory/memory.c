6814f90c5fe0012e9b9e0a5178902a34b14acb46
tracer-ics2017 <tracer@njuics.org>
2025-03-24 00:28:43 +0800

    >  compile 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:28:43 up 28 min,  1 user,  load average: 0.72, 0.20, 0.29 2f2f08cd89b6b3616f7418f4dd0995ca3ff31523

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -15,12 +15,7 @@ uint8_t pmem[PMEM_SIZE];
 uint32_t paddr_read(paddr_t addr, int len) {
   int map_NO = is_mmio(addr);
   if(map_NO == -1){
-    switch(len) {
-      case 1: return pmem_rw(addr, uint8_t);
-      case 2: return pmem_rw(addr, uint16_t);
-      case 4: return pmem_rw(addr, uint32_t);
-      default: Assert(0, "invalid memory read length %d", len);
-    }
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
   }
   else{
     return mmio_read(addr, len, map_NO);
@@ -30,12 +25,7 @@ uint32_t paddr_read(paddr_t addr, int len) {
 void paddr_write(paddr_t addr, int len, uint32_t data) {
   int map_NO = is_mmio(addr);
   if(map_NO == -1){
-    switch(len) {
-      case 1: pmem_rw(addr, uint8_t) = (uint8_t)data; break;
-      case 2: pmem_rw(addr, uint16_t) = (uint16_t)data; break;
-      case 4: pmem_rw(addr, uint32_t) = data; break;
-      default: Assert(0, "invalid memory write length %d", len);
-    }
+    memcpy(guest_to_host(addr), &data, len);
   }
   else{
     mmio_write(addr, len, data, map_NO);
