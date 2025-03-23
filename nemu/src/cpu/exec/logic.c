c0ae30d7939c2b07f407c738eaead553d55b0839
tracer-ics2017 <tracer@njuics.org>
2025-03-23 13:50:00 +0800

    >  compile 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:50:00 up 58 min,  1 user,  load average: 0.56, 0.26, 0.27 91c80a02498c14c8211f6ae7fc00fe32bd898c51

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -3,10 +3,10 @@
 // 两个操作数相与，根据结果设置eflags，不保存结果
 make_EHelper(test) {
   //TODO();
-  rtl_and(&t0, &id_dest->val, &id_src->val);
+  rtl_and(&t2, &id_dest->val, &id_src->val);
 
   // 更新零标志位(ZF)和符号标志位(SF)
-  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   // 进位标志位(CF)和溢出标志位(OF)置零
   rtl_set_CF(&tzero);
@@ -16,11 +16,11 @@ make_EHelper(test) {
 
 make_EHelper(and) {
   //TODO();
-  rtl_and(&t0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &t0);
+  rtl_and(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
 
   // 更新零标志位(ZF)和符号标志位(SF)
-  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   // 进位标志位(CF)和溢出标志位(OF)置零
   rtl_set_CF(&tzero);
@@ -30,11 +30,11 @@ make_EHelper(and) {
 
 make_EHelper(xor) {
   //TODO();
-  rtl_xor(&t0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &t0);
+  rtl_xor(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
   
   // 更新零标志位(ZF)和符号标志位(SF)
-  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
   
   // 逻辑运算清除进位标志位(CF)和溢出标志位(OF)
   rtl_set_CF(&tzero);
@@ -44,11 +44,11 @@ make_EHelper(xor) {
 
 make_EHelper(or) {
   //TODO();
-  rtl_or(&t0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &t0);
+  rtl_or(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
 
   // 更新零标志位(ZF)和符号标志位(SF)
-  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   // 逻辑运算清除进位标志位(CF)和溢出标志位(OF)
   rtl_set_CF(&tzero);
@@ -60,11 +60,11 @@ make_EHelper(or) {
 make_EHelper(sar) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_sext(&t0, &id_dest->val, id_dest->width);
-  rtl_sar(&t0, &t0, &id_src->val);
-  operand_write(id_dest, &t0);
+  rtl_sext(&t2, &id_dest->val, id_dest->width);
+  rtl_sar(&t2, &t2, &id_src->val);
+  operand_write(id_dest, &t2);
   // 更新零标志位(ZF)和符号标志位(SF)
-  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
   print_asm_template2(sar);
 }
 
@@ -72,10 +72,10 @@ make_EHelper(sar) {
 make_EHelper(shl) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_shl(&t0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &t0);
+  rtl_shl(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
   // 更新零标志位(ZF)和符号标志位(SF)
-  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(shl);
 }
@@ -84,14 +84,31 @@ make_EHelper(shl) {
 make_EHelper(shr) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_shr(&t0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &t0);
+  rtl_shr(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
   // 更新零标志位(ZF)和符号标志位(SF)
-  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_update_ZFSF(&t2, id_dest->width);
 
   print_asm_template2(shr);
 }
 
+// 循环左移
+// 等价于逻辑左移n位和逻辑右移(width * 8 - n)位进行或运算
+make_EHelper(rol) {
+  rtl_shl(&t2, &id_dest->val, &id_src->val);
+  rtl_shri(&t1, &id_dest->val, id_dest->width * 8 - id_src->val);
+  rtl_or(&t2, &t2, &t1);
+
+  operand_write(id_dest, &t2);
+
+  // 更新进位标志位(CF)
+  // t1 和 1 进行与运算，得到最低位的结果
+  rtl_li(&t2, 1);
+  rtl_and(&t2, &t1, &t2);
+  rtl_set_CF(&t2);
+  
+  print_asm_template2(rol);
+}
 
 make_EHelper(setcc) {
   uint8_t subcode = decoding.opcode & 0xf;
