068c21d112e8cb1a8f700bfecf1fd22ffe97f8ac
tracer-ics2017 <tracer@njuics.org>
2025-04-15 00:18:21 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:18:21 up  4:34,  1 user,  load average: 0.43, 0.44, 0.58 1af6c8307604823497c1c372739ffa317c778ea8

--- nanos-lite/include/ramdisk.h
+++ nanos-lite/include/ramdisk.h
@@ -1,15 +1,10 @@
 #ifndef RAMDISK_H
 #define RAMDISK_H
 
-
-// 声明外部变量
 extern uint8_t ramdisk_start[], ramdisk_end[];
 
-// 声明宏
 #define RAMDISK_SIZE ((size_t)(ramdisk_end - ramdisk_start))
 
-// 声明函数
 void ramdisk_read(void *buf, off_t offset, size_t len);
-void ramdisk_write(const void *buf, off_t offset, size_t len);
 
 #endif
