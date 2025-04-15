784838a6684ca8cd519f9751d6e313a2b7a8088a
tracer-ics2017 <tracer@njuics.org>
2025-04-15 17:57:11 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:57:11 up  2:43,  1 user,  load average: 0.85, 0.63, 0.53 dffe6a7687d5ee9a60b8381dad151b38d7c029c4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,7 +1,10 @@
 #include "common.h"
-#include "ramdisk.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
+
+extern uint8_t ramdisk_start,ramdisk_end;
+#define RAMDISK_SIZE ((size_t)(ramdisk_end - ramdisk_start))
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
