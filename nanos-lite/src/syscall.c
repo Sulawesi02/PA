f011aa7302fb1aaa93c1c6fa72d02319e5e28d50
tracer-ics2017 <tracer@njuics.org>
2025-04-16 18:24:57 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:24:57 up  1:49,  1 user,  load average: 0.15, 0.10, 0.03 81f19e84ef52a99ffc6073fb69460d4d919dbe98

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -33,7 +33,8 @@ _RegSet* do_syscall(_RegSet *r) {
       sys_exit(a[1]);
       break;
     case SYS_write:
-      SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);
+      //SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);
+      printf("111");
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
