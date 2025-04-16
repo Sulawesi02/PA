18ced523ca719ed2bfbaa824bf1b55a885fd31a1
tracer-ics2017 <tracer@njuics.org>
2025-04-17 02:43:20 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:43:20 up  8:48,  1 user,  load average: 0.37, 0.50, 0.84 6de2d83420f0d6b8b49d4d2a4d02ef1abd31a1b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -31,7 +31,7 @@ extern void dispinfo_read(void *buf, off_t offset, size_t len);
 void init_fs() {
   // TODO: initialize the size of /dev/fb
   file_table[FD_FB].size = _screen.height * _screen.width * 4;
-  Log("FD_FB size=%d",file_table[FD_FB].size);
+  //Log("FD_FB size=%d",file_table[FD_FB].size);
 }
 
 size_t fs_filesz(int fd) {
@@ -48,54 +48,96 @@ int fs_open(const char *pathname, int flags, int mode) {
   return -1;
 }
 
-ssize_t fs_read(int fd, void *buf, size_t len) {
-  assert(fd >= 0 && fd < NR_FILES);
-  int size = fs_filesz(fd) - file_table[fd].open_offset;// 文件剩余大小
-  if (size > len) {// 偏移量不能超过文件大小
-    size = len;
-  }
-  switch(fd) {
-    case FD_STDIN:
-    case FD_STDOUT:
-    case FD_STDERR:
-    case FD_FB:
-      return 0;
-    case FD_DISPINFO:// 屏幕信息
-      dispinfo_read(buf, file_table[fd].open_offset, len);
-      file_table[fd].open_offset += len;
-      break;
-    default:
-      ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, size);
-      file_table[fd].open_offset += size;
-  }
-  return size;
+ssize_t fs_read(int fd,void* buf,size_t len)
+{
+    //Log("Debug: cd fs_read");
+    assert(fd>0 && fd<NR_FILES);
+    if(fd<3)
+    {
+        Log("fd < 3 when use fs_read/n");
+        return 0;
+    }
+    //注意偏移量不要越过 文件的边界
+    int maxbyte=file_table[fd].size-file_table[fd].open_offset;
+    if(len>maxbyte)
+    {
+        //FD_DISPINFO 调用dispinfo_read;
+        if(fd==FD_DISPINFO)
+        {
+            //Log("len>maxbyte fd==FD_DISPINFO");
+            dispinfo_read(buf,file_table[fd].open_offset,maxbyte);
+        }
+        else//其他文件
+        {
+            ramdisk_read(buf,
+                file_table[fd].disk_offset+file_table[fd].open_offset,
+                maxbyte);
+        }
+        //更新偏移量
+        file_table[fd].open_offset+=maxbyte;
+        return maxbyte;
+    }
+    else
+    {
+        if(fd==FD_DISPINFO)
+        {
+            //Log("len< maxbyte fd==FD_DISPINFO");
+            dispinfo_read(buf,file_table[fd].open_offset,len);
+        }
+        else
+        {
+             ramdisk_read(buf,
+             file_table[fd].disk_offset+file_table[fd].open_offset,
+             len);
+        }
+        //更新偏移量
+        file_table[fd].open_offset+=len;
+        return len;
+    }
 }
 
-ssize_t fs_write(int fd, const void *buf, size_t len) {
-  assert(fd >= 0 && fd < NR_FILES);
-  int size = fs_filesz(fd) - file_table[fd].open_offset;// 文件剩余大小
-  if (size > len) {// 偏移量不能超过文件大小
-    size = len;
-  }
-  switch(fd) {
-    case FD_STDIN:
-      return 0;
-    case FD_STDOUT:
-    case FD_STDERR:
-      for (int i = 0; i < len; i++) {
-        _putc(((char *)buf)[i]);
-      }
-      break;
-    case FD_FB:// 显存
-      fb_write(buf, file_table[fd].open_offset, len);
-      file_table[fd].open_offset += len;
-      break;
-    default:
-      ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, size);
-      file_table[fd].open_offset += size;
-      break;
-  }
-  return size;
+ssize_t fs_write(int fd,void *buf, size_t len)
+{
+    //Log("Debug: cd fs_write");
+    assert(fd>0 && fd<NR_FILES);
+    if(fd<3)
+    {
+        Log("fd < 3 when use fs_write/n");
+        return 0;
+    }
+    int maxbyte=file_table[fd].size-file_table[fd].open_offset;
+    if(len>maxbyte)
+    {
+        if(fd==FD_FB)//VGA
+        {
+            fb_write(buf,file_table[fd].open_offset,maxbyte);
+        }
+        else
+        {
+            ramdisk_write(buf,
+                        file_table[fd].disk_offset+file_table[fd].open_offset,
+                        maxbyte);
+        }
+        //更新偏移量
+        file_table[fd].open_offset+=maxbyte;
+        return maxbyte;
+    }
+    else
+    {
+        if(fd==FD_FB)//VGA
+        {
+            fb_write(buf,file_table[fd].open_offset,len);
+        }
+        else
+        {
+            ramdisk_write(buf,
+                            file_table[fd].disk_offset+file_table[fd].open_offset,
+                            len);
+        }
+        //更新偏移量
+        file_table[fd].open_offset+=len;
+        return len;
+    }
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence) {
