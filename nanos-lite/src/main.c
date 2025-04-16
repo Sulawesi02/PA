1fcd02004c1a0dc786ce225aff49865752d2ec57
tracer-ics2017 <tracer@njuics.org>
2025-04-17 00:57:22 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:57:22 up  7:02,  1 user,  load average: 0.33, 0.20, 0.12 94a0c6b30d257e7642be9725a75d21d4113d0b

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -31,6 +31,7 @@ int main() {
   init_fs();
 
   //uint32_t entry = loader(NULL, NULL);
+  Log("Loading program from ramdisk...");
   uint32_t entry = loader(NULL, "/bin/text");
   ((void (*)(void))entry)();
 
