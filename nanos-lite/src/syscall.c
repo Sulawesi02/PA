65884db14230736447539d8fce2f30e2695a0723
tracer-ics2017 <tracer@njuics.org>
2025-04-16 17:10:03 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:10:03 up 34 min,  1 user,  load average: 0.05, 0.11, 0.15 4bcc5d45c23524cfcbaf1ed02bfa5a0c1cc54a3a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,16 @@
 #include "common.h"
 #include "syscall.h"
 
+int sys_write(int fd, void *buf, size_t n){
+  if(fd == 1 || fd == 2){
+    for(int i = 0; i < n; i++){
+      _putc(*((char*)buf + i));
+    }
+    return n;
+  }
+  return -1;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -14,6 +24,9 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_exit:
       _halt(a[1]);
+      break;
+    case SYS_write:
+      SYSCALL_ARG1(r) = sys_write(a[1], (void*)a[2], a[3]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
