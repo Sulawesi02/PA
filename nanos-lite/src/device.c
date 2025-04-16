9daf0a7c6568b71bdc6e43d4b9d265f4d96f1374
tracer-ics2017 <tracer@njuics.org>
2025-04-17 02:44:26 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:44:26 up  8:49,  1 user,  load average: 1.30, 0.75, 0.90 e75b1c0562c2e7fea6b77e5a6bae1a8f3bed93a3

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,42 +19,26 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-  //Log("cd fb_write");
-  if(offset%4!=0||len%4!=0)
-  {
-      panic("VGA 1像素 should be 4 byte");
+  int index = offset / 4;
+  int x1 = index % _screen.width;// 开始的列号
+  int y1 = index / _screen.width;// 开始的行号
+  int x2 = (index + len / 4) % _screen.width;// 结束的列号
+  int y2 = (index + len / 4) / _screen.width;// 结束的行号
+  if(y2 == y1) {
+    _draw_rect((uint32_t *)buf, x1, y1, x2 - x1, 1);
+    return;
   }
-  int width=_screen.width;
-  int index = offset/sizeof(uint32_t);//第多少个像素
-  //x1 ,y1开始的位置
-  int screen_y1 = index/width;
-  int screen_x1 = index%width;
-  //x2,y2结束的位置
-  index=(offset+len)/sizeof(uint32_t);
-  int screen_y2=index/width;
-  int screen_x2 = index%width;
-
-  if(screen_y2==screen_y1)
-  {
-      _draw_rect(buf,screen_x1,screen_y1,screen_x2-screen_x1,1);
-      return;
+  else if(y2 - y1 == 1) {
+    _draw_rect((uint32_t *)buf, x1, y1, _screen.width - x1, 1);
+    _draw_rect((uint32_t *)buf + 4 * (_screen.width - x1), 0, y2, x2, 1);
+    return;
   }
-  else if(screen_y2-screen_y1==1)
-  {
-      _draw_rect(buf,screen_x1,screen_y1,width-screen_x1,1);
-      _draw_rect(buf+4*(width-screen_x1),0,screen_y2,screen_x2,1);
-      return;
+  else{
+    _draw_rect((uint32_t *)buf, x1, y1, _screen.width - x1, 1);
+    _draw_rect((uint32_t *)buf + 4*(_screen.width - x1), 0, y1 + 1, _screen.width, y2 - y1 - 2);
+    _draw_rect((uint32_t *)buf + 4*(_screen.width - x1 + _screen.width*(y2 - y1 - 2)), 0, y2, x2, 1);
+    return;
   }
-  else if(screen_y2-screen_y1>1)
-  {
-      _draw_rect(buf,screen_x1,screen_y1,width-screen_x1,1);
-      int draw_index=width-screen_x1;
-      _draw_rect(buf+draw_index*4,0,screen_y1+1,width,screen_y2-screen_y1-2);
-      draw_index += width*(screen_y2-screen_y1-2);
-      _draw_rect(buf+draw_index*4,0,screen_y2,screen_x2,1);
-      return;
-  }
-  panic("screen_y2<screen_y1! erro");
 }
 
 void init_device() {
