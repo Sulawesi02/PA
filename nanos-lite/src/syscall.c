3b423eb2a2484f923248dcc1598ef27aaa90ebf8
tracer-ics2017 <tracer@njuics.org>
2025-04-16 22:29:28 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:29:28 up  4:34,  1 user,  load average: 0.01, 0.06, 0.08 45ed5ba62d9a48355a21b781cfcd339336b9f003

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,6 +3,7 @@
 
 int sys_write(int fd,void *buf,size_t len){
   if(fd == 1 || fd == 2){
+    Log("sys_write called: fd=%d, len=%d", fd, len);
     for(int i = 0; i < len; ++i){
       _putc(*((char *)buf + i));
     }
@@ -26,6 +27,7 @@ _RegSet* do_syscall(_RegSet *r) {
       _halt(a[1]);
       break;
     case SYS_write:
+      Log("SYS_write received: fd=%d, buf=0x%x, len=%d", a[1], a[2], a[3]);
       SYSCALL_ARG1(r) = sys_write(a[1], (void*)a[2], a[3]);
       break;
     case SYS_brk:
