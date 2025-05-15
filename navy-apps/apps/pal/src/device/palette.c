a0ee98330d4e6b07fee9cba07f1abccf44ceeb36
tracer-ics2017 <tracer@njuics.org>
2025-05-15 20:35:23 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:35:23 up  6:24,  1 user,  load average: 0.93, 0.61, 0.66 90d924bdf87f419a9617df2ede3534cbe42164f4

--- navy-apps/apps/pal/src/device/palette.c
+++ navy-apps/apps/pal/src/device/palette.c
@@ -217,20 +217,18 @@ PAL_FadeIn(
    //
    // Get the new palette...
    //
-   Log("PAL_FadeIn: palette %d, night %d, delay %d\n", iPaletteNum, fNight, iDelay);
    palette = PAL_GetPalette(iPaletteNum, fNight);
 
    //
    // Start fading in...
    //
-   Log("PAL_FadeIn: start fading in...\n");
    time = SDL_GetTicks() + iDelay * 10 * 60;
-   Log("PAL_FadeIn: time = %d\n", time);
    while (TRUE)
    {
       //
       // Set the current palette...
       //
+      Log("time = %d, iDelay = %d, j = %d\n", time, iDelay, j);
       j = (int)(time - SDL_GetTicks()) / iDelay / 10;
       if (j < 0)
       {
