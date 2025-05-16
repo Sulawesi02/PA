3a999b1b5f1f44b58a98bca9e7dccdb28e86ceed
tracer-ics2017 <tracer@njuics.org>
2025-05-16 18:19:53 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:19:53 up  3:45,  1 user,  load average: 0.64, 0.63, 0.50 800ba14eaa42111732160125e5914fb731d43d5d

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -66,7 +66,7 @@ void _switch(_Protect *p) {
 }
 
 void _map(_Protect *p, void *va, void *pa) {
-  uint32_t *pgdir = p->ptr;//页目录表基址
+  PDE *pgdir = p->ptr;//页目录表基址
   uint32_t pd_idx = (uint32_t)va >> 22;// 页目录索引
   uint32_t pt_idx = (uint32_t)va >> 12 & 0x3ff;// 页表索引
   
@@ -76,7 +76,7 @@ void _map(_Protect *p, void *va, void *pa) {
         ((uint32_t *)(pgdir[pd_idx]))[i] = 0;
     pgdir[pd_idx] = (uint32_t)pgtab | PTE_P;
   }
-  uint32_t *pgtab = (PTE *)(pgdir[pd_idx] & ~0xfff);// 页表基址
+  PTE *pgtab = (PTE *)(pgdir[pd_idx] & ~0xfff);// 页表基址
   pgtab[pt_idx] = (uint32_t)pa | PTE_P;
 }
 
