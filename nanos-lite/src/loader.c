9509b7fa7d356d42e76bfa50b15936c9f0607e5c
tracer-ics2017 <tracer@njuics.org>
2025-05-16 18:01:15 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:01:15 up  3:26,  1 user,  load average: 0.44, 0.28, 0.35 579ea01fcfb91ed26a96f3279d4d258ff125d993

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-#define DEFAULT_ENTRY ((void *)0x4000000)
+#define DEFAULT_ENTRY ((void *)0x8048000)
 
 // extern uint8_t ramdisk_start,ramdisk_end;
 // #define RAMDISK_SIZE ((&ramdisk_end)-(&ramdisk_start))
@@ -14,22 +14,25 @@ int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
-  // ramdisk_read(DEFAULT_ENTRY,0,RAMDISK_SIZE);//PA3.1
-
-  int fd=fs_open(filename,0,0);
-  Log("filename=%s,fd=%d",filename,fd);
-  //fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
-  int size=fs_filesz(fd);
-  int page_num=(size+PGSIZE-1)/PGSIZE;//页面数量
-
-  void *pa=NULL;
-  void *va=DEFAULT_ENTRY;//虚拟空间
-  for(int i=0;i<page_num;++i){//不断根据虚拟地址读取物理页
-    pa=new_page();//申请物理页
-    _map(as,va,pa);//建立映射
-    fs_read(fd,pa,PGSIZE);//读物理页
-    va+=PGSIZE;
+  // ramdisk_read(DEFAULT_ENTRY, 0, RAMDISK_SIZE);
+  int fd = fs_open(filename, 0, 0);
+  if(fd == -1){
+    Log("open file failed");
+    return 0;
   }
+  // Log("filename=%s,fd=%d",filename,fd);
+  // fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  int size = fs_filesz(fd);// 获取文件大小
+  int page_num = (size + PGSIZE - 1) / PGSIZE;// 页面数
+
+  void *pa, *va = DEFAULT_ENTRY;
+  for(int i = 0; i < page_num; i++){
+    pa = new_page();
+    _map(as, va, pa);
+    fs_read(fd, pa, PGSIZE);
+    va += PGSIZE;
+  }
+  
   fs_close(fd);
 
   return (uintptr_t)DEFAULT_ENTRY;
