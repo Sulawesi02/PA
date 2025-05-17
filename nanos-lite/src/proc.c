b53bbed48dff705d91c5de16424b36fdbf48d13f
tracer-ics2017 <tracer@njuics.org>
2025-05-17 21:19:44 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:19:44 up  4:28,  1 user,  load average: 0.41, 0.52, 0.72 b21fae2dab3ac4014d513ced1c0f9af8b6501fff

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,8 +30,8 @@ _RegSet* schedule(_RegSet *prev) {
   if(current != NULL){
     current->tf = prev;
   }
-  current = &pcb[0];
-  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  // current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   _switch(&current->as);
   return current->tf;
 }
