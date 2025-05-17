b3055d1f91a6ee07e37a4f735aeb3ef04995d97e
tracer-ics2017 <tracer@njuics.org>
2025-05-17 20:58:26 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:58:26 up  4:06,  1 user,  load average: 0.13, 0.66, 0.88 ccd3cc27e58ed89ff7ef4d5bf11252829722dbda

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,10 +5,10 @@ extern _RegSet* schedule(_RegSet *prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
-      do_syscall(r);
-      return schedule(r);
+      return do_syscall(r);
     case _EVENT_TRAP:
       printf("EVENT_TRAP\n");
+      do_syscall(r);
       return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
