45894b06d8b131af81f788faf1663020daa177d4
tracer-ics2017 <tracer@njuics.org>
2025-05-18 14:50:44 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  14:50:44 up 24 min,  1 user,  load average: 1.96, 2.02, 1.55 5d387f2e23593b7489589e65eb88f09e61eed6f

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,37 +9,25 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-    //return 0;
-    int key=_read_key();
-    bool down = false;
-    if (key & 0x8000) {
-          key ^= 0x8000;
-          down = true;
-    }
-    char temp[20];
-    if(down && key == _KEY_F12) {
-        extern int current_game;
-        current_game = (current_game == 0 ? 2 : 0);
-        Log("key down:_KEY_F12, switch current game!");
-     }
-    if (key != _KEY_NONE)
-    {
-      if(down){
-          sprintf(temp,"kd %s\n",keyname[key]);}
-      else{
-          sprintf(temp,"ku %s\n",keyname[key]);}
-    }
-    else{
-      sprintf(temp,"t %d\n",_uptime());}
-  
-    if(strlen(temp)<=len)
-    {
-      strncpy((char*)buf,temp,len);
-      return strlen(temp);
-    }
-    Log("strlen(event)=%d>len=%d",strlen(temp),len);
-    return 0;
+  int key = _read_key();
+  bool is_down = false;
+  if (key & 0x8000) {
+    key ^= 0x8000;
+    is_down = true;
   }
+  if(key == _KEY_F12 && is_down){// 按下F12，切换游戏
+    extern int current_game;
+    current_game = (current_game == 0 ? 2 : 0);
+    Log("切换游戏到 %s", current_game ? "videotest" : "pal");
+  } 
+  if(key == _KEY_NONE){
+    sprintf(buf, "t %d\n", _uptime());//时钟事件
+  }
+  else {
+    sprintf(buf, "%s %s\n", is_down?"kd":"ku", keyname[key]);//按键事件
+  }
+  return strlen(buf);
+}
 
 static char dispinfo[128] __attribute__((used));
 
