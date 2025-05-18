7114f7c75381991a222bc7530cbf301c25f0f790
tracer-ics2017 <tracer@njuics.org>
2025-05-18 15:20:06 +0800

    >  run 2213410 xu Linux xu-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:20:06 up 53 min,  1 user,  load average: 1.32, 1.78, 1.70 d82c79a467bd39a82136c1ec67f0466c80911091

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,32 +28,33 @@ void load_prog(const char *filename) {
 
 int current_game = 0; // 当前游戏的进程号
 _RegSet* schedule(_RegSet *prev) {
-    if(current!=NULL){
-      current->tf = prev;
-    }
-    else{
-      current = &pcb[current_game];
-    }
-    // current = &pcb[0];
-    // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-    static int count=0;
-    static const int max_count=100;
-    if(current==&pcb[current_game])
-    {
-        if(current_game==0)
-          Log("run pal %d\n",count);
-        else if(current_game==2)
-          Log("run videotest %d\n",count);
-         count++;
-    }
-    else{
-      current=&pcb[current_game];
-    }
-    if(count==max_count){
-      current=&pcb[1];
-      Log("run hello \n");
-      count=0;
-    }
-    _switch(&current->as);
-    return current->tf;
+  if(current != NULL){
+    current->tf = prev;
   }
+  else{
+    current = &pcb[current_game];
+  }
+  // current = &pcb[0];
+  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  static int count = 0;
+  static const int max_count = 1000;
+  if(current == &pcb[current_game])
+  {
+      if(current_game == 0)
+        Log("run pal %d\n",count);
+      else if(current_game == 2)
+        Log("run videotest %d\n",count);
+       count++;
+  }
+  else{
+    current = &pcb[current_game];
+  }
+  if(count == max_count){
+    current = &pcb[1];
+    Log("run hello \n");
+    count = 0;
+  }
+  
+  _switch(&current->as);
+  return current->tf;
+}
